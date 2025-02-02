986168c45baad552a02c2a83d147b88be8191210
1lent <brodiecuff@outlook.com>
2025-02-03 00:16:34 +0800

    now get tree in inventory

--- PyFiles/main.py
+++ PyFiles/main.py
@@ -1,83 +1,31 @@
 from ursina import *
+from player import Player
 from ursina.prefabs.first_person_controller import FirstPersonController
+
 
 import time
 
-shift = False
+
 lastPlacedBlock: int = 0
+shift = False
+pause = False
 
 app = Ursina()
 
-class Player(Entity):
-    def __init__(self, **kwargs):
-        self.controller = FirstPersonController(**kwargs)
-        super().__init__(parent=self.controller)
-
-        self.stone = Entity(parent=self.controller.camera_pivot,
-                            scale=0.5,
-                            position=(1, -0.75, 1.2),
-                            rotation=Vec3(0, 170, 0),
-                            model='stone',
-                            texture='stone.jpg',
-                            visible=False)
-
-        self.wood = Entity(parent=self.controller.camera_pivot,
-                            scale=0.5,
-                            position=(1, -0.75, 1.2),
-                            rotation=Vec3(0, 170, 0),
-                            model='cube',
-                            texture='texture-of-wood-photo.jpg',
-                            visible=False)
-
-        self.dirt = Entity(parent=self.controller.camera_pivot,
-                           scale=0.25,
-                           position=(1, -0.5, 1.2),
-                           rotation=Vec3(0, 170, 0),
-                           model='dirt',
-                           texture='dirt_texture.jpg',
-                        #    color = rgb(128, 128, 0),
-                           visible=False)
-
-        self.inventory = [self.stone, self.dirt]
-        self.current_inventory = 0
-        self.switch_inventory()
-
-    def switch_inventory(self):
-        for i, v in enumerate(self.inventory):
-            if i == self.current_inventory:
-                v.visible = True
-            else:
-                v.visible = False
-
-    def input(self, key):
-        try:
-            self.current_inventory = int(key) - 1
-            self.switch_inventory()
-        except ValueError:
-            pass
-
-        if key == 'scroll up':
-            self.current_inventory = (self.current_inventory + 1) % len(self.inventory)
-            self.switch_inventory()
-        if key == 'scroll down':
-            self.current_inventory = (self.current_inventory - 1) % len(self.inventory)
-            self.switch_inventory()
-
-    def update(self):
-        self.controller.camera_pivot.y = 2 - held_keys['left control']
 
 
-def place_block(position = [0, 0, 0], texture = 'cube_white'):
+def place_block(position=[0, 0, 0], texture='cube_white'):
     global lastPlacedBlock
     current_time = time.time()
     if (current_time - lastPlacedBlock < 0.1):
         pass
     else:
-      Voxel(position, texture)
-      lastPlacedBlock = current_time
+        Voxel(position, texture)
+        lastPlacedBlock = current_time
+
 
 class Voxel(Button):
-    def __init__(self, position=(0, 0, 0), texture='white_cube'):
+    def __init__(self, position=(0, 0, 0), texture='cube_white'):
         super().__init__(parent=scene,
                          position=position,
                          model='cube',
@@ -87,51 +35,46 @@ class Voxel(Button):
                          highlight_color=color.red,
                          )
 
+for z in range(1):
+    for x in range(1):
+        voxel_1 = Voxel(position=(3,1,4), texture='texture-of-wood-photo')
+        voxel_2 = Voxel(position=(3, 2, 4), texture='texture-of-wood-photo')
 
 for z in range(8):
     for x in range(8):
         voxel = Voxel(position=(x, 0, z))
 
-class Voxel(Button):
-    def __init__(self, position=(0, 1, 0), texture='white_cube'):
-        super().__init__(parent=scene,
-                         position=position,
-                         model='cube',
-                         origin_y=.5,
-                         texture='texture-of-wood-photo.jpg',
-                         color=color.hsv(0, 0, random.uniform(.9, 1.0)),
-                         highlight_color=color.red,
-                         )
-
-
-for z in range(1):
-    for x in range(1):
-        voxel = Voxel(position=(x, 1, z))
-
 
 def input(key):
     global shift
+    global pause
 
     if held_keys['right mouse']:
         hit_info = raycast(camera.world_position, camera.forward, distance=5)
         if hit_info.hit:
             # Voxel(position=hit_info.entity.position + hit_info.normal)
-            place_block(position=hit_info.entity.position + hit_info.normal, texture = player.inventory[player.current_inventory].texture)
+            place_block(position=hit_info.entity.position + hit_info.normal,
+                        texture=player.inventory[player.current_inventory].texture)
     if key == 'right mouse down':
         hit_info = raycast(camera.world_position, camera.forward, distance=5)
         if hit_info.hit:
-            place_block(position=hit_info.entity.position + hit_info.normal, texture = player.inventory[player.current_inventory].texture)
+            place_block(position=hit_info.entity.position + hit_info.normal,
+                        texture=player.inventory[player.current_inventory].texture)
+
             # Voxel(position=hit_info.entity.position + hit_info.normal, texture = player.inventory[player.current_inventory].texture)
             # Assign the texture of held item to Voxel
 
-    if key == 'left mouse down' and mouse.hovered_entity:
+    if key == 'left mouse down':
+        if mouse.hovered_entity.texture.name == "texture-of-wood-photo.jpg":
+            player.inventory.append(player.wood)
         destroy(mouse.hovered_entity)
-        if mouse.hovered_entity and mouse.hovered_entity.texture == 'texture-of-wood-photo.jpg':
-            player.current_inventory.append(player.wood)
+
     if held_keys['left mouse']:
+        if mouse.hovered_entity.texture.name == "texture-of-wood-photo.jpg":
+            print("hi")
+            player.inventory.append(player.wood)
         destroy(mouse.hovered_entity)
-        if mouse.hovered_entity and mouse.hovered_entity.texture == 'texture-of-wood-photo.jpg':
-            player.current_inventory.append(player.wood)
+
 
 
     if key == 'left shift':  # shift controls
@@ -140,6 +83,9 @@ def input(key):
             player.speed = 2.5
         else:
             player.speed = 5
+
+    if key == 'escape':
+        application.paused = not application.paused
 
 
 player = Player(position=(0, 10, 0))
